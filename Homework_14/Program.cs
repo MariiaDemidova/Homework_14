51a17a724f893da5793e2ecaa16638951f0612c2
Mariia <mariiademidova@outlook.com>
2021-12-21 15:51:46 +0300

    Добавьте файлы проекта.

---
+++ Homework_14/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Kind = "cобака";
+            Cat cat = new Cat();
+            cat.Kind = "кошка";
+            dog.ShowInfo();
+            Console.WriteLine();
+            cat.ShowInfo();
+            Console.ReadKey();
+
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Kind { get; set; }
+        public Animal () { }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine("Я {0}", Kind);
+            Say();
+        }
+    }
+    class Dog : Animal
+    {
+        public string kind;
+        public override string Kind
+        {
+            get
+            {
+                return kind;
+            }
+            set
+            {
+                kind = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав"); 
+        }
+    }
+    class Cat : Animal
+    {
+        public string kind;
+        public override string Kind
+        {
+            get
+            {
+                return kind;
+            }
+            set
+            {
+                kind = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
